96efb5524ff171e8fa8a8aef820d5103284c47c8
joao <joao182cx@gmail.com>
2019-01-12 00:30:03 -0200

    Mudanças na classe MeuRetangulo. Mudanças nos atributos e no construtor. Exclusão dos métodos getters e setters desnecessários na classe, já que agora estão disponíveis através da herança da superclasse MinhaForma.

--- src/MeuRetangulo.java
+++ src/MeuRetangulo.java
@@ -1,15 +1,9 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class MeuRetangulo {
+public class MeuRetangulo extends MinhaForma {
 	
-	private int x1;
-	private int y1;
-	private int x2;
-	private int y2;
 	private boolean preenchido;
-	
-	private Color cor;
 	
 	public MeuRetangulo() {
 		
@@ -18,13 +12,8 @@ public class MeuRetangulo {
 	}
 
 	public MeuRetangulo(int x1, int y1, int x2, int y2, boolean preenchido, Color cor) {
-		super();
-		setX1(x1);
-		setY1(y1);
-		setX2(x2);
-		setY2(y2);
-		setCor(cor);
-		setPreenchido(preenchido);
+		super(x1, y1, x2, y2, cor);
+		this.preenchido = preenchido;
 	}
 	
 	public void desenha(Graphics g) {
@@ -39,52 +28,21 @@ public class MeuRetangulo {
 	}
 	
 	public int retornaXCimaEsquerda() {
-		return x1 < x2 ? x1 : x2;
+		return getX1() < getX2() ? getX1() : getX2();
 	}
 	
 	public int retornaYCimaEsquerda() {
-		return y1 < y2 ? y1 : y2;
+		return getY1() < getY2() ? getY1() : getY2();
 	}
 	
 	public int retornaAltura() {
-		return Math.abs(x1 - x2);
+		return Math.abs(getX1() - getX2());
 	}
 	
 	public int retornaLargura() {
-		return Math.abs(y1 - y2);
+		return Math.abs(getY1() - getY2());
 	}
 
-	public int getX1() {
-		return x1;
-	}
-
-	public void setX1(int x1) {
-		this.x1 = x1 >= 0 ? x1 : 0;
-	}
-
-	public int getY1() {
-		return y1;
-	}
-
-	public void setY1(int y1) {
-		this.y1 = y1 >= 0 ? y1 : 0;
-	}
-
-	public int getX2() {
-		return x2;
-	}
-
-	public void setX2(int x2) {
-		this.x2 = x2 >= 0 ? x2 : 0;
-	}
-
-	public int getY2() {
-		return y2;
-	}
-
-	public void setY2(int y2) {
-		this.y2 = y2 >= 0 ? y2 : 0;
-	}
 
 	public boolean isPreenchido() {
 		return preenchido;
@@ -93,17 +51,6 @@ public class MeuRetangulo {
 	public void setPreenchido(boolean preenchido) {
 		this.preenchido = preenchido;
 	}
-
-	public Color getCor() {
-		return cor;
-	}
-
-	public void setCor(Color cor) {
-		this.cor = cor;
-	}
-	
-	
-	
 	
 
 }
